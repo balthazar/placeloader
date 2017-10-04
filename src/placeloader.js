786e1ee1a9d446c411b1cc493a6fd63f94b4b33a
Balthazar Gronon <balthazar@uber.com>
2017-10-04 11:47:38 -0700

    chore(core): react 16

--- src/placeloader.js
+++ src/placeloader.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { TransitionMotion, spring } from 'react-motion'
 
 import InnerPlaceLoader from './inner'
--- src/post.js
+++ src/post.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import PlaceLoader from './placeloader'
 import Image from './image'
